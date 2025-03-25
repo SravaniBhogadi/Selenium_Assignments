f8af182beade21a0f48b2ea09b30c9259c0d424a
Sravani Bhogadi <sravanibhogadi630@gmail.com>
2025-03-25 12:49:54 +0530

    Update Assignment2.java

--- Intro/src/package1/package1/Assignment2.java
+++ Intro/src/package1/package1/Assignment2.java
@@ -2,11 +2,21 @@ package package1;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 
 public class Assignment1 {
 	public static void main(String args[]) {
 		WebDriver driver = new ChromeDriver();
-		driver.get("https://rahulshettyacademy.com/angularpractice/");
+		Actions action = new Actions(driver);
+		driver.get("https://www.myntra.com/");
+		String Women = driver.findElement(By.xpath("//a[@data-group ='women']"));
+		action.moveToElement(Women).perform();
+		driver.findElement(By.xpath("//li[@data-reactid = '195']"));
+		driver.findElement(By.xpath("//img[@title = 'Samhitas Women Floral Printed V-Neck Kurti']")).click();
+		driver.findElement(By.xpath("//span[@class = 'myntraweb-sprite pdp-whiteBag sprites-whiteBag pdp-flex pdp-center']")).click();
+		driver.findElement(By.xpath(""));
+		driver.findElement(By.xpath(""));
+		driver.findElement(By.xpath(""));
 		
 	}
 	
