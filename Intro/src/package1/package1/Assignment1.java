0a0214e062c454b89da8de2b77d3ab35ff56b719
Sravani Bhogadi <sravanibhogadi630@gmail.com>
2025-03-25 11:05:59 +0530

    Update Assignment1.java

--- Intro/src/package1/package1/Assignment1.java
+++ Intro/src/package1/package1/Assignment1.java
@@ -2,11 +2,21 @@ package package1;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.By;
 
 public class Assignment1 {
 	public static void main(String args[]) {
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://rahulshettyacademy.com/angularpractice/");
+		driver.findElement(By.xpath("//input[@class = 'form-control ng-pristine ng-invalid ng-touched']")).sendKeys("Sravani");
+		driver.findElement(By.xpath("//input[@class = 'form-control ng-pristine ng-invalid ng-touched']")).sendKeys("sravani@gmail.com");
+		driver.findElement(By.xpath("//input[@id = 'exampleInputPassword1']")).sendKeys("sravani@457");
+		driver.findElement(By.xpath("//input[@id = 'exampleCheck1']")).click();
+		driver.findElement(By.xpath("//select[@id = 'exampleFormControlSelect1']/option[text() = 'Female']")).click();
+		driver.findElement(By.xpath("//input[@type= 'date']")).sendKeys("17-05-1999");
+		driver.findElement(By.xpath("//input[@type= 'submit']")).click();
+		String message = driver.findElement(By.xpath("//div[@class= 'alert alert-success alert-dismissible']")).getText();
+		System.out.println(message);
 		
 	}
 	
