4ff17a19f7b60c375a79ac0c6de435ff01f6839d
Sravani Bhogadi <sravanibhogadi630@gmail.com>
2025-03-25 15:19:26 +0530

    Update Assignment2.java

--- Intro/src/package1/package1/Assignment2.java
+++ Intro/src/package1/package1/Assignment2.java
@@ -10,16 +10,29 @@ public class Assignment1 {
 		Actions action = new Actions(driver);
 		driver.get("https://www.myntra.com/");
 		String Women = driver.findElement(By.xpath("//a[@data-group ='women']"));
+		
+		String ethinic_wear = By.xpath("//li[@data-reactid = '195']");
+		String select_dress = By.xpath("//img[@title = 'Samhitas Women Floral Printed V-Neck Kurti']");
+		String add_to_bag = By.xpath("//span[@class = 'myntraweb-sprite pdp-whiteBag sprites-whiteBag pdp-flex pdp-center']");
+		String select_size = By.xpath("(//span[@class = 'size-buttons-inventory-left'])[1]");
+		String open_bag = By.xpath("//span[@data-reactid = '872']");
+		String place_order = By.xpath("//div[text() = 'PLACE ORDER']");
+		String price = By.xpath("//div[@class = 'itemComponents-base-price itemComponents-base-bold ']/div");
+		
 		action.moveToElement(Women).perform();
-		driver.findElement(By.xpath("//li[@data-reactid = '195']"));
-		driver.findElement(By.xpath("//img[@title = 'Samhitas Women Floral Printed V-Neck Kurti']")).click();
-		driver.findElement(By.xpath("//span[@class = 'myntraweb-sprite pdp-whiteBag sprites-whiteBag pdp-flex pdp-center']")).click();
-		driver.findElement(By.xpath(""));
-		driver.findElement(By.xpath(""));
-		driver.findElement(By.xpath(""));
+		String x_ethinic = driver.findElement(ethinic_wear).click();
+		String x_dress = driver.findElement(select_dress).click();
+		driver.findElement(add_to_bag).click();
+		char x_size = driver.findElement(select_size);.click();
+		driver.findElement(open_bag).click();
+		int x_price = driver.findElement(place_order).click();
+		
+		
+		System.out.println(x_ethinic.getText());
+		System.out.println(x_dress.getText());
+		System.out.println(x_size.getText());
+		System.out.println(x_price.getText());
 		
 	}
 	
-	
-
 }
