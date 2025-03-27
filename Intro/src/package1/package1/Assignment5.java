1e2c5660709813b17586d03fe64fa81542fc051e
SravaniBhogadi <sravanibhogadi630@gmail.com>
2025-03-27 07:33:26 +0530

    del

---
+++ Intro/src/package1/package1/Assignment5.java
@@ -0,0 +1,14 @@
+package package1;
+
+import org.openqa.selenium.WebDriver;
+
+public class Assignment5 {
+
+	public static void main(String[] args) {
+		WebDriver driver = new ChromDriver();
+		driver.get("https://rahulshettyacademy.com/loginpagePractise/");
+		
+		
+	}
+
+}
