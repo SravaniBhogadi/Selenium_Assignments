aab902d88c3a556949f260305f22f38de88f96cb
SravaniBhogadi <sravanibhogadi630@gmail.com>
2025-03-28 23:26:07 +0530

    practise

---
+++ Intro/src/Windowhandlings.java
@@ -0,0 +1,18 @@
+
+public class Windowhandlings {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		1.https://the-internet.herokuapp.com/
+			multiple windows
+		2. https://rahulshettyacademy.com/loginpagePractise	
+		
+		3. nested frames -- print middle text
+			
+			amazon -- mousehover actions
+			
+
+	}
+
+}
