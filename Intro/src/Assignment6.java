943fb84800793bee254fad15ebf8d2186eee6550
Sravani Bhogadi <sravanibhogadi630@gmail.com>
2025-03-29 17:55:51 +0530

    Update Assignment6.java

--- Intro/src/Assignment6.java
+++ Intro/src/Assignment6.java
@@ -1,1 +1,10 @@
+public class Assignment6 {
 
+	public static void main(String[] args) {
+		driver.get("https://rahulshettyacademy.com/loginpagePractise");
+		
+		webElement secondWindow = driver.findElement(By.xpath("//a[@class = 'blinkingText']");
+		secondWindow.click();
+
+		webElement text = driver.findElement(By.xpath("//p[@class='im-para red']"));
+		System.out.println(text.getText());
