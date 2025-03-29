f3e651b390fc0fdfdd126c57aebb7e90f103e6c7
Sravani Bhogadi <sravanibhogadi630@gmail.com>
2025-03-29 17:39:17 +0530

    Update Windowhandlings.java

--- Intro/src/Windowhandlings.java
+++ Intro/src/Windowhandlings.java
@@ -2,10 +2,22 @@
 public class Windowhandlings {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		driver.get("https://the-internet.herokuapp.com/");
 		
-		1.https://the-internet.herokuapp.com/
-			multiple windows
+		webElement multipleWindow = driver.findElement(By.xpath("//a[text() = 'Multiple Windows']");
+		multipleWindow.click();
+
+		webElement text = driver.findElement(By.xpath("//h3[text() = 'Opening a new window']"));
+		System.out.println(text.getText());
+
+		webElement second_window = driver.findElement(By.xpath("//a[text() = 'Click Here']");
+		second_window.click();
+		
+		webElement secon_window_text = driver.findElement(By.xpath("//div[@class = 'example']");
+		system.out.println(second_window_text.getText());
+
+		driver.switchTOWindow()
+			
 		2. https://rahulshettyacademy.com/loginpagePractise	
 		
 		3. nested frames -- print middle text
