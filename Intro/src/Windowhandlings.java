18c126c5f82dc14f5973de4e6fbf2c024a396436
SravaniBhogadi <sravanibhogadi630@gmail.com>
2025-04-03 22:51:30 +0530

    c2

--- Intro/src/Windowhandlings.java
+++ Intro/src/Windowhandlings.java
@@ -1,7 +1,11 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Windowhandlings {
 
 	public static void main(String[] args) {
+		WebDriver driver = new ChromeDriver();
 		driver.get("https://the-internet.herokuapp.com/");
 		
 		webElement multipleWindow = driver.findElement(By.xpath("//a[text() = 'Multiple Windows']");
@@ -20,11 +24,19 @@ public class Windowhandlings {
 			
 			
 		
-		3. nested frames -- print middle text
-			
-			amazon -- mousehover actions
-			
+
 
 	}
 
 }
+
+
+
+
+https://rahulshettyacademy.com/AutomationPractice/
+	
+	
+	
+	
+	
+	
