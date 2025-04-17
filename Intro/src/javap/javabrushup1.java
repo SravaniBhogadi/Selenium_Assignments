4d631cb59b3af46261f0eba624bd0f7e9dfa76a9
Sravani Bhogadi <sravanibhogadi630@gmail.com>
2025-04-17 17:17:35 +0530

    Update javabrushup1.java
    3

--- Intro/src/javap/javabrushup1.java
+++ Intro/src/javap/javabrushup1.java
@@ -31,7 +31,23 @@ class Main {
 }
 
 -------------------------
+MISSING NUMBER IN AN ARRAY
 
+class Main{
+    public static void main(String[] args){
+        int[] arr = {5,6,8,9};
+        int arr_sum = 0;
+        for(int i=0;i<arr.length;i++){
+            arr_sum += arr[i];
+        }
+        int original_arr_sum = 0;
+        for(int i =5;i<=9;i++){
+            original_arr_sum += i;
+        }
+        System.out.println("Missing element is " + 
+        (original_arr_sum - arr_sum));
+    }
+}
 
 
 
